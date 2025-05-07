2d68019f38452d930b16d2a86636467d154ad032
joko26 <playerone99@outlook.de>
2025-05-07 17:57:58 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (input.trim() === '') return;
+    setTodos([...todos, { text: input, done: true}]);
+    setInput('');
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const toggleDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].done = !newTodos[index].done;
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Meine To-Do-Liste</h1>
+      <input 
+      type="text" 
+      value={input} 
+      onChange={(e) => setInput(e.target.value)} 
+      placeholder="Neue Aufgabe">
+      </input>
+      <button onClick={handleAddTodo}>Hinzufügen</button>
+
+      <ul style={{ listStyle: 'none', padding: 0}}>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input 
+            type="checkbox"
+            checked="todo.done"
+            onChange={() => toggleDone(index)}>
+            </input>
+            <span style={{
+              textDecoration: todo.done ? 'line-through' : 'none', marginLeft: '8px'}}>
+                {todo.text}
+            </span>
+            <button onClick={() => handleDelete(index)} style={{marginLeft: '10px' }}> löschen
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
